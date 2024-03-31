37310f00ae2313881537cc07897dac157a93c2a9
fuzzice <kokotaki126@gmail.com>
2024-03-31 14:13:44 +0800

    update: cover

--- src/pages/redenvelope/index.vue
+++ src/pages/redenvelope/index.vue
@@ -1,16 +1,28 @@
 <template>
-  <div class="page-container py-5">
-    <div class="text-3xl font-bold text-red text-shadow-sm">Red Envelope</div>
+  <div class="page-container max-w-900px py-5">
+    <div class="flex justify-between">
+      <div class="text-3xl font-bold text-red text-shadow-sm">
+        <span>Red Envelope</span>
+      </div>
+      <span class="text-2xl font-bold" v-if="accountStore.profile?.telegramUser?.username">
+        Hi, {{ accountStore.profile?.telegramUser?.username }}
+      </span>
+      <Button v-else @click="loginGoogle">Sign In</Button>
+    </div>
 
-    <div class="grid grid-cols-2 md:grid-cols-3 gap-3 mt-10">
+    <div class="grid grid-cols-2 md:grid-cols-3 gap-10 mt-20 mx-auto">
       <div class="envelope" v-for="(n, index) in envelopeList" :key="index">
-        <span class="text-xs">Env. {{ n.index }}</span>
-        <span class="text-4xl font-bold py-3 truncate px-3 max-w-full break-all text-center">
-          {{ n.data?.[1] }}
-        </span>
-        <span>{{ n.data?.[3] }} / {{ n.data?.[2] }}</span>
+        <div
+          class="bg-white/90 py-10 flex-col items-center rounded-lg w-2/3 shadow-lg -translate-y-10"
+        >
+          <span class="text-xs">Env. {{ n.index }}</span>
+          <span class="text-4xl font-bold py-3 truncate px-3 max-w-full break-all text-center">
+            {{ n.data?.[1] }}
+          </span>
+          <span>{{ n.data?.[3] }} / {{ n.data?.[2] }}</span>
+        </div>
         <Button
-          class="!w-1/2 mt-10"
+          class="!w-1/3 aspect-1 absolute bottom-[1%] mt-15 !rounded-full opacity-0 cursor-pointer"
           v-if="n.data?.[3] != n.data?.[2]"
           @click="openEnvelopeHandler(n)"
         >
@@ -37,11 +49,13 @@
   import { CONTRACT_ADDRESS, FUZZ_LOGIN_JWT, KeylessAptosClient } from '@/config';
   import useKeyless from '@/hooks/useKeyless';
   import Button from '@/lib/Button.vue';
+  import useAccountStore from '@/store/AccountStore';
   import { message } from 'ant-design-vue';
   import { isEmpty } from 'lodash-es';
 
   const FuzzLoginJWT = localStorage.getItem(FUZZ_LOGIN_JWT) as string;
   const { loginGoogle } = useKeyless();
+  const accountStore = useAccountStore();
 
   const currentSelectIndex = ref(-1);
   const currentPayload = computed(() => {
@@ -107,9 +121,10 @@
 
 <style lang="less" scoped>
   .envelope {
-    @apply bg-red text-white py-3 hover:shadow-lg cursor-pointer gap-2;
-    @apply rounded-md p-[10px] w-full aspect-3/4;
+    @apply bg-red text-black py-3 hover:shadow-xl cursor-pointer gap-2;
+    @apply rounded-md p-[10px] w-full aspect-3/4 relative;
     @apply flex-col-center;
+    background: url('../../assets/images/re-cover.jpg') no-repeat center center / cover;
   }
 </style>
 
