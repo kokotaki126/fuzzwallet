da30e46d0a29ce1364ba132cc96dcee04217419e
fuzzice <kokotaki126@gmail.com>
2024-04-25 18:14:17 +0800

    test

--- src/pages/wallet/index.vue
+++ src/pages/wallet/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="!p-0">
+    <Button @click="requestAccess">Request</Button>
     <div class="px-4">
       <div class="grid grid-cols-2 gap-4 my-3">
         <Button class="!w-full !text-xl" @click="faucetHandler" :loading="fauceting">
@@ -38,6 +39,26 @@
       message.error(e.message || e);
     } finally {
       fauceting.value = false;
+    }
+  };
+
+  const requestAccess = async () => {
+    if (window.Telegram.WebApp.BiometricManager.isInited) {
+      alert('inited');
+      // window.Telegram.WebApp.BiometricManager.requestAccess();
+      window.Telegram.WebApp.BiometricManager.openSettings();
+    } else {
+      alert('go init');
+      window.Telegram.WebApp.BiometricManager.init([
+        (isInited: boolean) => {
+          console.log('isInited', isInited);
+          if (isInited) {
+            // window.Telegram.WebApp.BiometricManager.requestAccess();
+            // window.Telegram.WebApp.BiometricManager.openSettings();
+            alert('inited');
+          }
+        },
+      ]);
     }
   };
 </script>
